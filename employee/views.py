5967d36314709bc611dab1e1e27da343aecc46ca
sarmila831 <tharusarmila831@gmail.com>
2024-11-08 11:55:42 +0545

    Update views.py

--- employee/views.py
+++ employee/views.py
@@ -22,7 +22,7 @@ from . import views
 class UserAttendanceViewSet(viewsets.ModelViewSet):
     queryset = UserAttendance.objects.all()
     serializer_class = UserAttendanceSerializer
-
+    permission_classes = [IsAuthenticated]
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
