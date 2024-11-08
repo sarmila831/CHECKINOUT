56cfdc8317ccce388bbe8f22be767cbf22e42f91
sarmila831 <tharusarmila831@gmail.com>
2024-11-08 20:57:04 +0545

    Update views.py

--- employee/views.py
+++ employee/views.py
@@ -29,13 +29,38 @@ class UserAttendanceViewSet(viewsets.ModelViewSet):
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    
     def update(self, request, *args, **kwargs):
         attendance = self.get_object()
-        if attendance.check_in_time and not attendance.check_out_time:
+        '''if attendance.check_in_time and not attendance.check_out_time:
             attendance.check_out_time = timezone.now()
             attendance.save()
             serializer = self.get_serializer(attendance)
             return Response(serializer.data)
+        return Response({"detail": "Cannot check out without checking in."}, status=status.HTTP_400_BAD_REQUEST)'''
+       
+    
+    
+    # Check if there's a check_in_time and no check_out_time
+        if attendance.check_in_time and not attendance.check_out_time:
+           check_out_time = request.data.get('check_out_time')  # Get check_out_time from the request data
+        
+           if  check_out_time:
+            # Convert the string to a timezone-aware datetime object
+               check_out_time = timezone(timezone.datetime.fromisoformat(check_out_time.replace("Z", "+00:00")))
+               attendance.check_out_time = check_out_time
+               attendance.save()
+            
+            # Return the updated attendance data along with worked hours
+               serializer = self.get_serializer(attendance)
+               worked_hours = attendance.worked_hours()  # Recalculate worked hours
+               return Response({
+                'attendance': serializer.data,
+                'worked_hours': worked_hours  # Include the worked hours in the response
+            })
+           else:
+                return Response({"detail": "check_out_time is required."}, status=status.HTTP_400_BAD_REQUEST)
+    
         return Response({"detail": "Cannot check out without checking in."}, status=status.HTTP_400_BAD_REQUEST)
 
     def list(self, request, *args, **kwargs):
